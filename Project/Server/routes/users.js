71ba133544c7c3acc0787b0e4555c9ae00a94f2a
Syed Ameer Basha <ameerong7@gmail.com>
2026-03-12 00:04:54 +0530

    fix authorize import

--- Project/Server/routes/users.js
+++ Project/Server/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../middleware/auth');
-const authorize = require('../middleware/roles');
+const { authorize } = require('../middleware/roles');
 
 /**
  * User Management Routes
